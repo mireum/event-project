ed1f1c2a4051dc36648ba14893260aa25ddb4646
fkdkskdfjfk <yljw225@naver.com>
2023-11-06 17:20:42 +0900

    develope setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+import { Routes } from 'react-router-dom';
+
+
+const GlobalStyle = createGlobalStyle`
+  /* 글로벌 스타일 */
+  body {
+    box-sizing:  border-box;
+  }
+
+  * {
+    box-sizing: inherit;
+  }
+
+  .cursor-pointer {
+    cursor: pointer;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+
+      <Routes>
+        {/* 이 안에 Route 작성 */}
+      </Routes>
+    </>
   );
 }
 
