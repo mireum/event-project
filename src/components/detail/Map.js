57c1e24b16996799b5d9d3f902ff8ea12b59c1f4
yunminsu <msg3265@nave.com>
2023-11-08 14:37:45 +0900

    listitem 1차 업로드

--- src/components/detail/Map.js
+++ src/components/detail/Map.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { getEventItem } from '../../api/eventAPI';
 import MapItem from './MapItem';
-import TheaterLocation from './TheaterLocation';
 
 function Map(props) {
   const { EventListId } = useParams();
