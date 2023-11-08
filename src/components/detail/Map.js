ade9f17333aa391e1e5ca6b0e013923d86940c5d
yunminsu <msg3265@nave.com>
2023-11-08 16:30:43 +0900

    업로드

--- src/components/detail/Map.js
+++ src/components/detail/Map.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { getEventItem } from '../../api/eventAPI';
 import MapItem from './MapItem';
+import styled from 'styled-components';
+
+const MapContainer = styled.div`
+  margin: 0 auto;
+`;
 
 function Map(props) {
   const { EventListId } = useParams();
-  console.log(EventListId);
+  const mapList = getEventItem.filter(eventitem => EventListId === eventitem.id);
+  console.log(mapList);
 
   return (
     <>
