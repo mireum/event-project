8f4a8c341c0004ae62ee22772f09104bb719f7ac
fkdkskdfjfk <yljw225@naver.com>
2023-11-10 14:46:00 +0900

    코드 수정

--- src/components/list/EventList.js
+++ src/components/list/EventList.js
@@ -24,67 +24,16 @@ const MoreButton = styled(Button)`
 `;
 
 function EventList(props) {
-
   const subject = useSelector(searchSubject);
   const month = useSelector(searchMonth);
   const location = useSelector(searchLocation);
   const category = useSelector(searchCategory);
-
-
 
   const [ showList, setShowList ] = useState(12);
 
   const moreShow = () => {
     setShowList(showList + 6);
   }
-
-
-  // const filteredEventList = (subject && month && location && category) && 
-  //   getEventItem.filter((event) => {
-  //     return (
-  //       (event.유형===subject &&  event.축제시작일자.split('-')[1]===month && 
-  //       event.소재지도로명주소.split(' ')[0]===location &&
-  //       event.카테고리===category)
-  //     );
-  // })
-  // const filteredEventList = 
-  // subject 
-  // ? getEventItem.filter(event => event.유형 === subject) 
-  // : category
-  //   ? getEventItem.filter(event => event.카테고리 === category || event.카테고리 ===)
-
-  // const filteredEventList = getEventItem
-  // .filter(event => event.유형 === subject)
-  // .filter(event => event.축제시작일자.split('-')[1] == month)
-  // .filter(event => event.소재지도로명주소.split(' ')[0] == location)
-  // .filter(event => event.카테고리 === category);
-
-
-  // const filteredEventList = 
-  // subject || month || location || category
-  // ? getEventItem.filter(event => {
-  //   return (
-  //     event.유형 === subject &&
-  //     event.카테고리 === category &&
-  //     event.축제시작일자.split('-')[1] == month &&
-  //     event.소재지도로명주소.split(' ')[0] == location
-  //     );
-  //   })
-  //   : getEventItem;
-
-
-  // : category
-  //   ? getEventItem.filter(event => event.카테고리 === category || event)
-  //   : month
-  //     ? getEventItem.filter(event => event.축제시작일자.split('-')[1] == month)
-  //     : location
-  //       ? getEventItem.filter(event => event.소재지도로명주소.split(' ')[0] == location)
-  //       : getEventItem
-
-  const subject = useSelector(searchSubject);
-  const month = useSelector(searchMonth);
-  const location = useSelector(searchLocation);
-  const category = useSelector(searchCategory);
 
   const filteredEventList = getEventItem
   .filter(event => {
@@ -115,20 +64,12 @@ function EventList(props) {
           <MainDetailSearch />
         </DetailSearchStyle>
         <Row>
-          {/* {subject || month || location || category
-            ? filterList.map(item => <FestivalListItem key={item.id} item={item}/>).slice(0,showList) */}
-            { getEventItem.map(item => <EventListItem key={item.id} item={item}/>).slice(0,showList)}
-        </Row>
-      </StyledContainer>
-      { showList >= getEventItem.length 
-
           {filteredEventList.length > 1
             ? filteredEventList.map(item => <EventListItem key={item.id} item={item}/>).slice(0,showList)
             : getEventItem.map(item => <EventListItem key={item.id} item={item}/>).slice(0,showList)}
         </Row>
-      </Container>
-      { showList > filteredEventList.length
-
+      </StyledContainer>
+      { showList > filteredEventList.length || getEventItem.length
         ? null
         : 
         <MoreButton 
