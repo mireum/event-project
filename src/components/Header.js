668eff53b1771b20ed9bc46704d0f412315cf818
fkdkskdfjfk <yljw225@naver.com>
2023-12-11 17:24:51 +0900

    3

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,8 @@ import Finder from './Finder';
 import { Outlet, useNavigate } from 'react-router-dom';
 import Footer from './Footer';
 import logo from "../images/logo.png";
-<<<<<<< HEAD
 import axios from 'axios';
-=======
 import { AiOutlineUser } from "react-icons/ai";
->>>>>>> 4db7b289088122b4c694f7a56fbcdc3613db86ee
 
 
 const HeaderWrap = styled.header`
