629095a2e8014ff1331c36d8eced135cd496d00d
haeun82 <cjwd829@gmail.com>
2023-12-14 00:19:32 +0900

    데이터 받기 시도..

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { findOne } from 'domutils';
 import React, { useEffect, useState } from 'react';
 
 
@@ -13,45 +12,50 @@ function BoardList() {
   useEffect(() => {
     const getBoardList = async () => {
       try {
-        const response = await axios.post(`http://localhost:8088/board/list`, {
-          title: boardContent.title,
-          content: boardContent.content
-        })
-        console.log(response.title);
-        console.log(response.content);
+        const result = await axios.get(`http://localhost:8088/board/list`);
+        return setBoardList(result.data);
+        
       } catch (err) {
         console.error(err);
       }
     }
     getBoardList();
-    }, []);
+    }, [BoardList]);
   return (
     <section>
-      <div class="board_list">
+      <div className="board_list">
         <table>
           <thead>
-            <tr class="table_menu">
-              <th class="list_subject">제목</th>
-              <th class="list_writer">작성자</th>
-              <th class="list_date">작성일</th>
-              <th class="list_watch">조회수</th>
+            <tr className="table_menu">
+              <th className="list_subject">제목</th>
+              <th className="list_writer">작성자</th>
+              <th className="list_date">작성일</th>
+              <th className="list_watch">조회수</th>
             </tr>
-          </thead>
+          </thead> 
           <tbody>
-            <tr class="table_list">
-              <td class="list_subject" id="list_subject">
-                <a href="/boardRead?id={{board.id}}">{BoardList.title}</a>
-              </td>
-              <td class="list_writer" id="list_writer">{BoardList.content}</td>
-              <td class="list_date">작성일</td>
-              <td class="list_watch">조회수</td>
-            </tr>
-            </tbody>
-          </table>
-        <button>글 쓰기</button>
+            {BoardList.map((item, index) => {
+              return(
+                <tr className="table_list" key={index}>
+                  <td className="list_subject" id="list_subject">
+                    <a href="/boardRead?id={{board.id}}">{item.title}</a>
+                  </td>
+                  <td className="list_writer" id="list_writer">{item.content}</td>
+                  <td className="list_date">작성일</td>
+                  <td className="list_watch">조회수</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        {/* <button>글 쓰기</button> */}
       </div>
     </section>
   );
 }
 
 export default BoardList;
+
+{/* <tbody key={index}>
+  
+</tbody>  */}
