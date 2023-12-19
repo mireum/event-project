b4eff96754d05c87432807013532630ce111a91f
fkdkskdfjfk <yljw225@naver.com>
2023-12-19 10:51:31 +0900

    1

--- src/components/list/EventList.js
+++ src/components/list/EventList.js
@@ -77,9 +77,6 @@ function EventList(props) {
   const button = useSelector(searchButton);
 
 
-
-
-  console.log(eventLists[0]);
   // console.log(typeof eventLists[0].fstvlStartDate);
   let filteredEventList = [];
   
